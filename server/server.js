7f2d91fc09924ed307d308ac8771c326726a1227
Sweathabalakrishnan <142570136+Sweathabalakrishnan@users.noreply.github.com>
2026-04-10 12:16:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
+// import express from "express";
+// import cors from "cors";
+// import path from "path";
+// import { ROOT } from "./utils/paths.js";
+// import eventRoutes from "./routes/events.js";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/events", eventRoutes);
+// app.use("/uploads", express.static(path.join(ROOT, "uploads")));
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -6,14 +24,24 @@ import eventRoutes from "./routes/events.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://event-photo-finder-mu.vercel.app"
+  ]
+}));
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 app.use("/api/events", eventRoutes);
 app.use("/uploads", express.static(path.join(ROOT, "uploads")));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-
